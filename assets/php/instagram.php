dd9a903e610bdd8204fd2586033cbe3cd149a810
bashirmurtaza93 <bashirmurtaza93@gmail.com>
2017-05-18 11:46:57 -0400

    Added new videos to shows section On shows clicking on a picture will give you a video. No longer calling from the API

--- assets/php/instagram.php
+++ assets/php/instagram.php
@@ -1,5 +1,5 @@
 <?php
-function getInstagramPhotos(){
+function getInstagramMedia(){
   $token = '48128506.6d8ceff.687e9ca1da804f4db1e32c6acec6a953';
   $url = 'https://api.instagram.com/v1/users/self/media/recent/?count=12&access_token='.$token;
   $instagramInfo = curl($url);
@@ -7,17 +7,30 @@ function getInstagramPhotos(){
   foreach($instagramInfo['data'] as $info){
       $id = $info['id'];
       $instagramPhotos->$id = new stdClass;
-      $instagramPhotos->$id->image = $info['images']['standard_resolution']['url'];
+      if($info['type'] == 'image'){
+        $instagramPhotos->$id->image = $info['images']['standard_resolution']['url'];
+      }else{
+        $instagramPhotos->$id->image = $info['images']['standard_resolution']['url'];
+        $instagramPhotos->$id->video = $info['videos']['standard_resolution']['url'];
+      }
       $instagramPhotos->$id->url = $info['link'];
   }
   return $instagramPhotos;
 }
 
 function displayInstagramPhotos(){
-  $photos = getInstagramPhotos();
+  $photos = getInstagramMedia();
   foreach ($photos as $key => $photo) {
     echo '<div class="col-xs-12 col-sm-6 col-lg-4 p-b-50">
-            <a href="'.$photo->url.'" target="_blank"><img class="instagram-image col-xs-12" src="'.$photo->image.'"/></a>
+            <a href="'.$photo->url.'" target="_blank">';
+            if($photo->video){
+              echo '<video class="main-video" loop autoplay="autoplay">
+                <source src="'.$photo->video.'" type="video/mp4">
+              </video>';
+            }else{
+            echo'<img class="instagram-image col-xs-12" src="'.$photo->image.'"/>';
+          }
+            echo'</a>
           </div>';
   }
 }
