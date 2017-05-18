c2cd09c8ed65cdf058589be72025d46c05251ea9
Alejandra Jimenez <axjimenez89@gmail.com>
2017-05-18 14:02:49 -0400

    resized edc images

--- index.php
+++ index.php
@@ -85,40 +85,47 @@
         </div>
       </section>
 
-
       <section class="container content-section">
         <hr class="section-break">
         <h1 class="text-center"> Festival Activations </h1>
         <div class="image-container">
         <div class="row">
         <div class="col-xs-12 col-sm-12 cold-md-12">
-          <div class="col-xs-8 col-sm-4 col-lg-4">
+          <div class="col-xs-12 col-sm-6 col-lg-4">
+          <a href="http://lasvegas.electricdaisycarnival.com/" target="_blank">
+          <img class="festivals electric lasvegas" src="/assets/images/edc-lasvegas.png"/></a>
+          </div>
+          <div class="col-xs-12 col-sm-6 col-lg-4">
           <a href="http://www.springawakeningfestival.com/" target="_blank">
           <img class="festivals spring" src="/assets/images/springawakening.png"/></a>
           </div>
-          <div class="col-xs-8 col-sm-4 col-lg-4">
-          <a href="http://www.nocturnalwonderland.com/" target="_blank">
-          <img class="festivals nocturnal" src="/assets/images/nocturnal.png"/></a>
-          </div>
-          <div class="col-xs-8 col-sm-4 col-lg-4">
-          <a href="http://lasvegas.electricdaisycarnival.com/" target="_blank">
-          <img class="festivals electric" src="/assets/images/edc.png"/></a>
-          </div>
-          <div class="col-xs-8 col-sm-4 col-lg-4">
-          <a href="http://escapehalloween.com/" target="_blank">
-          <img class="festivals escape" src="/assets/images/escape.png"/></a>
-          </div>
-          <div class="col-xs-8 col-sm-4 col-lg-4">
+          <div class="col-xs-12 col-sm-6 col-lg-4">
           <a href="http://hardfest.com/hardsummer" target="_blank">
           <img class="festivals hard" src="/assets/images/HARD1.png"/></a>
           </div>
-          <div class="col-xs-8 col-sm-4 col-lg-4">
-          <a href="http://orlando.electricdaisycarnival.com/" target="_blank">
-          <img class="festivals electric" src="/assets/images/edcORL.png"/></a>
-              </div>
+          <div class="col-xs-12 col-sm-6 col-lg-4">
+           <div class="row-2">
+          <a href="http://www.nocturnalwonderland.com/" target="_blank">
+          <img class="festivals nocturnal" src="/assets/images/nocturnal.png"/></a>
+          </div>
+          </div>
+          <div class="col-xs-12 col-sm-6 col-lg-4">
+            <div class="row-2">
+          <a href="http://escapehalloween.com/" target="_blank">
+          <img class="festivals escape" src="/assets/images/escape.png"/></a>
             </div>
-          </div
-       </div>
+          </div>
+          
+          <div class="col-xs-12 col-sm-6 col-lg-4">
+            <div class="row-2">
+          <a href="http://orlando.electricdaisycarnival.com/" target="_blank">
+          <img class="festivals electric orlando" src="/assets/images/edcORL.png"/></a>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
+     
       </section>
    
 
